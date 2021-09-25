fb0430d9158579f92c21391164dfef89009178e3
Jorge-dedcells <78442071+Jorge-dedcells@users.noreply.github.com>
2021-09-24 19:30:21 -0500

    Initial commit

---
+++ PM_1_ProgramasClase_U1/P0_Intro.asm
@@ -0,0 +1,27 @@
+TITLE Introduccion
+
+;DESCRPICIÓN
+;Objetivo: Archivo de Ejemplo
+;
+; Autore(s):
+; Mtro. Alejandro Garcia
+; Su Nombre.!
+;
+
+; Semestre: 8vo Semestre ISC
+;FIN DESCRIPCIÓN
+
+INCLUDE Irvine32.inc
+
+.data
+; Área de Declaración de Variables
+
+.code
+
+ inicia PROC
+;Lógica del Programa
+
+
+exit
+inicia ENDP
+END inicia
---
+++ PM_1_ProgramasClase_U1/P1_Variables.asm
@@ -0,0 +1,27 @@
+TITLE Introduccion
+
+;DESCRPICIÓN
+;Objetivo: Archivo de Ejemplo
+;
+; Autore(s):
+; Mtro. Alejandro Garcia
+; Su Nombre Jorge Alberto Casanova Gomez.!
+;
+
+; Semestre: 8vo Semestre ISC
+;FIN DESCRIPCIÓN
+
+INCLUDE Irvine32.inc
+
+.data
+; Área de Declaración de Variables
+
+.code
+
+ main1 PROC
+;Lógica del Programa
+
+
+exit
+main1 ENDP
+END main1
